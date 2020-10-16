dd93fd6e92d10aa485a5e5d6090e0d2b8bccc5f0
jack <wangxichao001@gmail.com>
2020-10-16 10:46:40 +0800

    Update sortTwoSortableArr.js
    两个升序数组合并为一个升序数组; add description

--- src/code/sort/sortTwoSortableArr.js
+++ src/code/sort/sortTwoSortableArr.js
@@ -1,9 +1,15 @@
 const a = [1,3,5,7,9]
 const b = [2,4,6,8]
 
+/**
+ * 两个升序数组合并为一个升序数组
+ * @param {Array} a [1, 3, 5, 7, 9]
+ * @param {Array} b [2, 4, 6, 8]
+ * @return {Array} [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ */
 const sort = (a = [], b = []) => {
-  let i = j = 0, c = [], a_length = a.length, b_length = b.length
-  while(i<a_length || j<b_length){
+  let i = j = 0, c = [], aLength = a.length, bLength = b.length
+  while(i < aLength || j < bLength){
       if(b[j] > a[i] && a[i] || !b[j]){
           c.push(a[i])
           i++
